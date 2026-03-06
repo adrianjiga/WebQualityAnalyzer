9ef50f6c07dfd3ee292505c0a99967827a6acf74
Adrian Jiga <jiga.ion.adrian@gmail.com>
2026-03-07 00:28:29 +0200

    test: trigger code review workflow

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,4 @@
 // Background script for WebQualityAnalyzer extension
 
 // Listen for extension installation
-chrome.runtime.onInstalled.addListener((): void => {});
+chrome.runtime.onInstalled.addListener((): void => {});// debug test
